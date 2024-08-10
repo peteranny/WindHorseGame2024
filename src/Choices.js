50eb3e9391e3b9b0e93d921dbb9c48b7ac9a1957
Peteranny <peteranny2@gmail.com>
2024-08-10 11:14:15 +0800

    non-randomly find speed choice

--- src/Choices.js
+++ src/Choices.js
@@ -4,7 +4,7 @@ import usePrev from './usePrev';
 
 const GET_CHOICES = ({ fp, setFp, setFpmax, speed, setSpeed, setSuperSec, setVelocityDiff, setGravity, hasHorse }) => [
   (() => {
-    const speeddiff = randomChoice([-1, -0.5, -0.3].filter(v => speed + v >= 1));
+    const speeddiff = [-5, -4, -3, -2, -1, -0.5, -0.3].find(v => speed + v >= 1);
     if (!speeddiff) return null;
     return {
       desc: fnum("橫移速度", "🥾", speeddiff),
